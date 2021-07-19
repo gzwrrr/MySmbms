6cba8a42d52559e2a210dfa88f71fa29311edcb1
HonestFox101 <952508490@qq.com>
2021-07-19 23:12:34 +0800

    完善ProviderService

--- src/main/Java/com/gzw/service/provider/ProviderServiceImpl.java
+++ src/main/Java/com/gzw/service/provider/ProviderServiceImpl.java
@@ -23,30 +23,94 @@ public class ProviderServiceImpl extends GeneralSqlService implements ProviderSe
 
     @Override
     public List<Provider> getProviderList(String proName, String proCode) {
-        return null;
+        List<Provider> providerList = null;
+
+        try (SqlSession sqlSession = sqlSessionFactory.openSession())
+        {
+            Provider provider = new Provider();
+            provider.setProCode(proCode);
+            provider.setProName(proName);
+
+            providerList = sqlSession.selectList("mybatis.mapper.ProviderMapper.selectProvider", provider);
+            sqlSession.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return providerList;
     }
 
     @Override
     public int deleteProviderById(String delId) {
-        return 0;
+        int id = 0;
+        try(SqlSession sqlSession = sqlSessionFactory.openSession()){
+            id = Integer.parseInt(delId);
+
+            Provider provider = new Provider();
+            provider.setId(id);
+
+            sqlSession.delete("mybatis.mapper.ProviderMapper.deleteProvider",provider);
+            sqlSession.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return id;
     }
 
     @Override
     public Provider getProviderById(String id) {
-        return null;
+        Provider provider = null;
+
+        try (SqlSession sqlSession = sqlSessionFactory.openSession())
+        {
+            Provider queryProvider = new Provider();
+            queryProvider.setId(Integer.parseInt(id));
+
+            provider = sqlSession.selectOne("mybatis.mapper.ProviderMapper.selectProvider", queryProvider);
+            sqlSession.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return provider;
     }
 
     @Override
     public boolean modify(Provider provider) {
-        return false;
+        if(provider.getId() == null){
+            return false;
+        }
+        try(SqlSession sqlSession = sqlSessionFactory.openSession()){
+            if (sqlSession.selectOne("mybatis.mapper.ProviderMapper.selectProvider",provider) == null){
+                return false;
+            }
+            sqlSession.update("mybatis.mapper.ProviderMapper.updateProvider",provider);
+            sqlSession.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Test
-    public void TestAddProvider() {
-        Provider provider = new Provider();
-        provider.setId(14);
-        provider.setProCode("GZ_10086");
+    public void TestGetProviderList() {
+        List<Provider> list = getProviderList("","GZ");
+        for (Provider provider :
+                list) {
+            System.out.println(provider.getProName());
+        }
+    }
 
-        add(provider);
+    @Test
+    public void TestDeleteProvider(){
+        deleteProviderById("0");
+    }
+
+    @Test
+    public void TestModifyProvider(){
+        Provider provider = new Provider();
+        provider.setId(15);
+        provider.setProContact("孙欣欣");
+
+        System.out.println(modify(provider));
     }
 }
