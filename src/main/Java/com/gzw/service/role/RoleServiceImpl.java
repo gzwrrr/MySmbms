6bffefb56472cebdf901769fb62a5aed0bdd34c2
HonestFox101 <952508490@qq.com>
2021-07-19 17:51:39 +0800

    更新RoleServiceImpl.java

--- src/main/Java/com/gzw/service/role/RoleServiceImpl.java
+++ src/main/Java/com/gzw/service/role/RoleServiceImpl.java
@@ -1,45 +1,45 @@
 package com.gzw.service.role;
 
-import com.gzw.dao.BaseBao;
-import com.gzw.dao.role.RoleDao;
-import com.gzw.dao.role.RoleDaoImpl;
 import com.gzw.pojo.Role;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.converter.ConvertWith;
 
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class RoleServiceImpl implements RoleService {
 
+    private static final String configPath = "mybatis/mybatis-config.xml";
+    private SqlSessionFactory sqlSessionFactory;
 
-    private RoleDao roleDao;
-
-    public RoleServiceImpl(){
-        roleDao = new RoleDaoImpl();
+    public RoleServiceImpl() throws IOException{
+        // 读取配置文件
+        InputStream config  = Resources.getResourceAsStream(configPath);
+        // 通过配置文件构建SqlSessionFactory
+        this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(config);
     }
 
     @Override
     public List<Role> getRoleList() {
-        Connection connection = null;
-        List<Role> roleList = null;
-        try {
-            connection = BaseBao.getConnection();
-            roleList = roleDao.getRoleList(connection);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            BaseBao.closeResource(connection, null, null);
-        }
+        // 通过SqlSessionFactory创建SqlSession
+        SqlSession sqlSession = this.sqlSessionFactory.openSession();
+        // 将查询到的信息放到列表
+        List<Role> roleList = sqlSession.selectList("mybatis.mapper.RoleMapper.selectAllRole");
+        // 提交并关闭
+        sqlSession.commit();
+        sqlSession.close();
         return roleList;
     }
 
     @Test
     public void test(){
-        RoleServiceImpl roleService = new RoleServiceImpl();
-        List<Role> roleList = roleService.getRoleList();
-        System.out.println(Arrays.toString(roleList.toArray()));
+        List<Role> roleList = getRoleList();
+        for (Role role: roleList) {
+            System.out.println(role.getRoleName());
+        }
     }
 }
