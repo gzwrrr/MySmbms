30dde3b4d8f052afa09aed0e37936c1eccb2d9fa
HonestFox101 <952508490@qq.com>
2021-07-20 15:46:17 +0800

    Implement addBill

--- src/main/Java/com/gzw/service/bill/BillServiceImpl.java
+++ src/main/Java/com/gzw/service/bill/BillServiceImpl.java
@@ -1,15 +1,32 @@
 package com.gzw.service.bill;
 
 import com.gzw.pojo.Bill;
+import com.gzw.service.abstractService.GeneralSqlService;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class BillServiceImpl  implements BillService{
-
+public class BillServiceImpl extends GeneralSqlService implements BillService{
 
     @Override
     public boolean add(Bill bill) {
-        return false;
+        if (bill.getId() == null){
+            return false;
+        }
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            Bill queryBill = new Bill();
+            queryBill.setId(bill.getId());
+            if(sqlSession.selectOne("mybatis.mapper.BillMapper.selectBill",queryBill) != null){
+                return false;
+            }
+            sqlSession.insert("mybatis.mapper.BillMapper.insertBill", bill);
+            sqlSession.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -30,5 +47,15 @@ public class BillServiceImpl  implements BillService{
     @Override
     public boolean modify(Bill bill) {
         return false;
+    }
+
+    @Test
+    public void TestAddBill(){
+        BillService billService = new BillServiceImpl();
+        Bill bill = new Bill();
+        bill.setId(20);
+        bill.setBillCode("10086");
+        boolean result = billService.add(bill);
+        System.out.println(result);
     }
 }
