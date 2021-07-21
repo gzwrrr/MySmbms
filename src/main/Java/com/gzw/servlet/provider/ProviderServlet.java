3f59a99e36c1ff1914e96e61b69f8d141c0b2bd7
Tyrfly <1071131161@qq.com>
2021-07-21 20:50:15 +0800

    Merge remote-tracking branch 'origin/dev' into devzjr
    # Conflicts:
    #	src/main/Java/com/gzw/servlet/bill/BillServlet.java
    #	target/MySmbms-1.0-SNAPSHOT.war

--- src/main/Java/com/gzw/servlet/provider/ProviderServlet.java
+++ src/main/Java/com/gzw/servlet/provider/ProviderServlet.java
@@ -8,34 +8,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProviderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String method = req.getParameter("method");
-        if ("query".equals(method)){
+        if ("query".equals(method))
          this.query(req,resp);
-
-        }
          else if("add".equals(method))
              this.add(req,resp);
+         else if("delprovider".equals(method))
+             this.delete(req,resp);
+
     }
 
     private void add(HttpServletRequest req, HttpServletResponse resp) throws  ServletException ,IOException{
         Object[] paras = req.getParameterValues("paras");
+//        System.out.println(Arrays.toString(paras));
+
         Provider provider = new Provider();
         provider.setProvider(paras,req,resp);
         ProviderServiceImpl providerService = new ProviderServiceImpl();
+
         boolean flag=providerService.add(provider);
         if(flag==true)
-        {
             req.setAttribute("message","添加成功！");
-        }else  req.setAttribute("message","添加失败请重试");
+        else  req.setAttribute("message","添加失败请重试");
         req.getRequestDispatcher("provideradd.jsp").forward(req,resp);
     }
+    private boolean delete(HttpServletRequest req, HttpServletResponse resp) throws  ServletException ,IOException
+    {
 
-    private void query(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException {
+     List<Provider >providers=query(req,resp);
+     Provider provider= providers.get(0);
+        ProviderServiceImpl providerService = new ProviderServiceImpl();
+       int flag=providerService.deleteProviderById( provider.getId().toString());
+        if(flag!=0)
+            return true;
+        else return  false;
+    }
+
+    private  List<Provider> query(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException {
         String queryProCode = req.getParameter("queryProCode");
         String queryProName = req.getParameter("queryProName");
         String pageIndexinfo = req.getParameter("pageIndex");
@@ -62,7 +77,7 @@ public class ProviderServlet extends HttpServlet {
         req.setAttribute("currentPageNo",currentPageNo);
         req.setAttribute("totalPageCount",totalPageCount);
         req.getRequestDispatcher("providerlist.jsp").forward(req,resp);
-
+return  providerList;
     }
 
     @Override
