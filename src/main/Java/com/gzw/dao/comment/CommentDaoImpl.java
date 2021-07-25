3425b998eec294650cb62a2a0274ac96bb1ab2e7
HonestFox101 <952508490@qq.com>
2021-07-25 16:44:33 +0800

    fix structure of CommentDaoImpl

--- src/main/Java/com/gzw/dao/comment/CommentDaoImpl.java
+++ src/main/Java/com/gzw/dao/comment/CommentDaoImpl.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CommentDaoImpl implements CommentDao {
@@ -14,67 +15,63 @@ public class CommentDaoImpl implements CommentDao {
 
     @Override
     public boolean add(Connection connection, Comment comment) {
-      int flag=0;
+        int flag=0;
+        if(connection == null)
+            return false;
         PreparedStatement preparedStatement=null;
         Object[] paras = {comment.getContent(),comment.getUserId(),comment.getGoodId()};
-        String sql="insert into smbms_comment(content,userId,goodId)values(?,?,?);";
+        String sql="insert into smbms_comment (content,userId,goodId) values(?,?,?)";
         try {
             flag=BaseBao.execute(connection,sql,paras,preparedStatement);
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
              BaseBao.closeResource(null,preparedStatement,null);
         }
-    if (flag>0)
-        return  true;
-    else return  false;
+        return flag > 0;
     }
 
     @Override
     public boolean delete(Connection connection, Comment comment) {
         int flag=0;
+
         PreparedStatement preparedStatement=null;
-        Object []paras={comment.getCommentId()};
-        String sql="delete from smbms_comment where commentId=?;";
+        Object[] paras={comment.getCommentId()};
+        String sql="delete from smbms_comment where commentId=?";
         try {
             flag=BaseBao.execute(connection,sql,paras,preparedStatement);
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
-            BaseBao.closeResource(null,preparedStatement,null);
+            BaseBao.closeResource(null, preparedStatement,null);
         }
-        if (flag>0)
-            return  true;
-        else return  false;
+        return flag > 0;
     }
-    public List<Comment> query(Connection connection, Integer goodId)
-    {
-        List<Comment> commentList=null;
-        Comment comment=null;
-        ResultSet resultSet=null;
-        PreparedStatement preparedStatement=null;
-        Object[]paras={goodId};
-        String sql="select * from smbms_comment where commentId=?;";
-        if(connection!=null) {
-            try {
-                resultSet = BaseBao.execute(connection, preparedStatement, resultSet, sql, paras);
-                while(resultSet.next())
-                {
-                    comment.setCommentId(resultSet.getInt("commentId"));
-                    comment.setContent(resultSet.getString("content"));
-                    comment.setUserId(resultSet.getInt("userId"));
-                    comment.setGoodId(resultSet.getInt("goodId"));
-            commentList.add(comment);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }finally {
-                {
-                    BaseBao.closeResource(null,preparedStatement,resultSet);
-                }
+    public List<Comment> query(Connection connection, Integer goodId) {
+        List<Comment> commentList = new ArrayList<>();
+        Comment comment = new Comment();
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        Object[] paras= {goodId};
+        String sql="select * from smbms_comment where commentId=?";
+        if(connection == null)
+            return commentList;
+        try {
+            resultSet = BaseBao.execute(connection, preparedStatement, resultSet, sql, paras);
+            while(resultSet.next())
+            {
+                comment.setCommentId(resultSet.getInt("commentId"));
+                comment.setContent(resultSet.getString("content"));
+                comment.setUserId(resultSet.getInt("userId"));
+                comment.setGoodId(resultSet.getInt("goodId"));
+                commentList.add(comment);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            BaseBao.closeResource(null, preparedStatement, resultSet);
         }
-     return  commentList;
+        return commentList;
     }
 
     @Override
